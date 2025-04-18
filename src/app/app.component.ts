4277231e8a489fdd4224894b06cbcd0cd163fe84
Fahad Bajwa <fahadbajwa1149@gmail.com>
2025-04-18 12:42:37 +0500

    Understanding Component concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './first-component/first.component';
+import { SecondComponent } from './second/second.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FirstComponent, SecondComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'my-firstangular-project';
 }
