6873393e763bfc97b39a9a3fea7072ffa6a49f94
Fahad Bajwa <fahadbajwa1149@gmail.com>
2025-04-22 12:44:01 +0500

    Created 2 new components

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+
 import { AppComponent } from './app/app.component';
-import {TestSCSSStylingsComponent} from './app/test-scss-stylings/test-scss-stylings.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
-
-bootstrapApplication(TestSCSSStylingsComponent); // This isn't a good and recommended approach(Calling bootstrapApplication() more than once).
-
-// More Information mentioned at Point # 1 -> "Info/PE-7498-Task.txt"
-
-
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
