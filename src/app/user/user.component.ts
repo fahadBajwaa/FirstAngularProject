fcd6ced17a643549127c4272c8a839d2a5eeb996
Fahad Bajwa <fahadbajwa1149@gmail.com>
2025-04-22 12:44:50 +0500

    Storing Data in a Component Class

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 //Created a helper function for getting a random index for DUMMY_USERS array.
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);  //Math.floor here rounds the number down to the nearest whole number. Math.random provides value in between 0 and 1(return floating-point number).
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);   //Math.floor here rounds the number down to the nearest whole number. Math.random provides value in between 0 and 1(return floating-point number).
 
 @Component({
   selector: 'app-user',
@@ -13,5 +13,5 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);  //Math.floo
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUsers = DUMMY_USERS[randomIndex]; // this line declares a class property named selectedUsers and initializes it with a value from the DUMMY_USERS array. In TypeScript, when defining properties within a class, we don't use let or const. These keywords are reserved for variable declarations within function or block scopes.
+  selectedUsers = DUMMY_USERS[randomIndex];  // this line declares a class property named selectedUsers and initializes it with a value from the DUMMY_USERS array. In TypeScript, when defining properties within a class, we don't use let or const. These keywords are reserved for variable declarations within function or block scopes.
 }
