325673f62a49c1da22b48b00d9745dbda679333e
Fahad Bajwa <fahadbajwa1149@gmail.com>
2025-04-22 15:25:01 +0500

    Managing State and Changing Data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -19,12 +19,13 @@ export class UserComponent {
   //Here, I will declare a "getter" which will perform some computation and use in template file as a property.
   // its essentially a method, it's a function isnside of a class that's usable like a property, it produce and return a new value
   get imagePath() {
-
     return 'assets/users/' + this.selectedUsers.avatar; //  used this keyword, to access the property of a class from within that same class.
   }
 
   // method for event
-  onSelectUser(){
-    console.log("User clicked")
+  onSelectUser() {
+    // console.log("User clicked")
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length); //Math.floor here rounds the number down to the nearest whole number. Math.random provides value in between 0 and 1(return floating-point number).
+    this.selectedUsers = DUMMY_USERS[randomIndex];
   }
 }
