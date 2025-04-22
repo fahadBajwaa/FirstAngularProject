57b44b90443edbffebe9bd3b50f8bf2135bcff86
Fahad Bajwa <fahadbajwa1149@gmail.com>
2025-04-22 11:21:00 +0500

    Angular Component Concepts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import { bootstrapApplication } from '@angular/platform-browser';  // Standalone API to bootstrap my root component
-import { appConfig } from './app/app.config'; // App‑wide providers (router, change detection, etc.)
-import { AppComponent } from './app/app.component'; // my root component
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 // import { SecondComponent } from './app/second/second.component';
 import {TestSCSSStylingsComponent} from './app/test-scss-stylings/test-scss-stylings.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => // Bootstraps AppComponent with the provided configuration
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
 
 // bootstrapApplication(SecondComponent);
-bootstrapApplication(TestSCSSStylingsComponent);
+bootstrapApplication(TestSCSSStylingsComponent); // This isn't a good and recommended approach(Calling bootstrapApplication() more than once).
+
+// More Information mentioned at Point # 1 -> "Info/PE-7498-Task.txt"
 
 
