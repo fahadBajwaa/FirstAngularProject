683367654b10a6b1c0f40e1f7208ba1b3fde7f35
Fahad Bajwa <fahadbajwa1149@gmail.com>
2025-04-22 14:11:02 +0500

    Using Getters for Computed Values

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,4 +15,11 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length); //Math.floor
 export class UserComponent {
   // private selectedUsers = DUMMY_USERS[randomIndex];  // In case of private property, it will not be available to template but only within the class.
   selectedUsers = DUMMY_USERS[randomIndex]; // this line declares a class property named selectedUsers and initializes it with a value from the DUMMY_USERS array. In TypeScript, when defining properties within a class, we don't use let or const. These keywords are reserved for variable declarations within function or block scopes.
+
+  //Here, I will declare a "getter" which will perform some computation and use in template file as a property.
+  // its essentially a method, it's a function isnside of a class that's usable like a property, it produce and return a new value
+  get imagePath() {
+
+    return 'assets/users/' + this.selectedUsers.avatar; //  used this keyword, to access the property of a class from within that same class.
+  }
 }
